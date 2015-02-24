df6c5619e07d00435ce86bf42e727b4cea0a91e2
Jordi Llobet <jordi.llobet@me.com>
2015-02-24 23:10:37 +0000

    Clean some code and created a readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Circular-visualization
-Object that generates a simple circular visualization.  
+
+![Circular-percentage](http://s1.postimg.org/x59zrod27/Captura_de_pantalla_2015_02_24_a_las_23_06_36.png)
+
+This is an js object that lets you build a visualizacion to show percentage data, or other data that can be represented by a circular visualization. The plugin has been created using the library [D3.js](http://d3js.org/).
+
+###Installation
+The file *circular-percentage.js* has to be added after add the D3 library on your html file. The object will be initializate automatically, so you don't need to call it. On the next step I'll explain how to pass the parameters to draw the correct circle.
+```html
+<script src="assets/js/lib/d3.v3.min.js"></script>
+<script src="assets/js/lib/circular-percentage.js"></script>
+```
+
+###How to use
+
+To send the parameters to the object, you need to call the **init** funcion. And the parameters are:
+- ID of the element where the visualization will be placed
+- **Minimum** value of the visualization
+- **Maximum** value of the visualization
+- **Final value** we want to show
+- **Class** will be assigned to the circles. This option is basically for css.
+
+```html
+<script type="text/javascript">
+  createAverage.init("circular_graph", 0, 100, 85, 'circle');
+</script>
+```
+
---
+++ assets/js/lib/circular-percentage.js
@@ -0,0 +1,75 @@
+var createAverage = (function() {
+
+	var svgWidth = 300,
+		iWidthCircle = 50,
+		τ = 2 * Math.PI,
+		fontSize = "2em",
+		arc, svg, background, foreground, text, avg, max;
+
+	
+	function init(elem, min, max, avg, class_assign)
+	{
+		avg = avg;
+		max = max;
+
+		arc = d3.svg.arc()
+			.innerRadius(iWidthCircle)
+			.outerRadius(iWidthCircle+3)
+			.startAngle(0);
+
+		svg = d3.select("#"+elem).append("svg")
+			.attr("width", svgWidth)
+			.attr("height", svgWidth)
+			.attr("class","svg_cercles")
+			.append("g")
+			.attr("transform", "translate(" + svgWidth/2 + "," + svgWidth/2 +")");
+
+		background = svg.append("path")
+			.datum({endAngle: τ})
+			//.attr('class', 'path_background')
+			.attr('class', class_assign+'_back')
+			.attr("d", arc);
+
+		foreground = svg.append("path")
+			.datum({endAngle: τ})
+			//.attr('class', 'path_foreground')
+			.attr('class', class_assign+'_front')
+			.attr("d", arc);
+
+		svg.append("text")
+			.attr("text-anchor", "middle")
+			.attr("font-size", fontSize)
+			.attr("dy", ".35em")
+
+		text = svg.select("text")
+			.attr('class','txt_cercle');
+
+		foreground.transition()
+			.duration(1000)
+			.call(animation, (avg * τ/max), max)
+			.each("end", myCallback);
+	}
+
+	function animation(transition, newAngle, max)
+	{
+		transition.attrTween("d", function(d) {
+			var interpolate = d3.interpolate(0, newAngle);
+			var interpolateValue = d3.interpolateNumber(0, newAngle);
+			return function(t) {				
+				d.endAngle = interpolate(t);
+				var textLabels = text
+					.text( d3.format(".1f")(interpolateValue(t) * max / τ) +' %' );
+				return arc(d);
+			};
+		});
+	}
+	
+	function myCallback() {
+		foreground.transition().duration(500).ease("back-out").attr("transform", "scale(2.1)");
+		background.transition().duration(500).ease("back-out").attr("transform", "scale(2.1)");
+	}
+
+	return {
+		init
+	};
+})();
