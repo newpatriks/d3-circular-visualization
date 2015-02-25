1ec89ecf719cd8e94d62b63f44f1799ab60b8f1d
jordillobet <newpatriks@gmail.com>
2015-02-25 10:22:20 +0000

    Fixed problem with Chrome

--- assets/js/lib/circular-percentage.js
+++ assets/js/lib/circular-percentage.js
@@ -6,50 +6,6 @@ var createAverage = (function() {
 		fontSize = "2em",
 		arc, svg, background, foreground, text, avg, max;
 
-	
-	function init(elem, min, max, avg, class_assign)
-	{
-		avg = avg;
-		max = max;
-
-		arc = d3.svg.arc()
-			.innerRadius(iWidthCircle)
-			.outerRadius(iWidthCircle+3)
-			.startAngle(0);
-
-		svg = d3.select("#"+elem).append("svg")
-			.attr("width", svgWidth)
-			.attr("height", svgWidth)
-			.attr("class","svg_cercles")
-			.append("g")
-			.attr("transform", "translate(" + svgWidth/2 + "," + svgWidth/2 +")");
-
-		background = svg.append("path")
-			.datum({endAngle: τ})
-			//.attr('class', 'path_background')
-			.attr('class', class_assign+'_back')
-			.attr("d", arc);
-
-		foreground = svg.append("path")
-			.datum({endAngle: τ})
-			//.attr('class', 'path_foreground')
-			.attr('class', class_assign+'_front')
-			.attr("d", arc);
-
-		svg.append("text")
-			.attr("text-anchor", "middle")
-			.attr("font-size", fontSize)
-			.attr("dy", ".35em")
-
-		text = svg.select("text")
-			.attr('class','txt_cercle');
-
-		foreground.transition()
-			.duration(1000)
-			.call(animation, (avg * τ/max), max)
-			.each("end", myCallback);
-	}
-
 	function animation(transition, newAngle, max)
 	{
 		transition.attrTween("d", function(d) {
@@ -70,6 +26,47 @@ var createAverage = (function() {
 	}
 
 	return {
-		init
-	};
+		init : function(elem, min, max, avg, class_assign)
+		{
+			avg = avg;
+			max = max;
+
+			arc = d3.svg.arc()
+				.innerRadius(iWidthCircle)
+				.outerRadius(iWidthCircle+3)
+				.startAngle(0);
+
+			svg = d3.select("#"+elem).append("svg")
+				.attr("width", svgWidth)
+				.attr("height", svgWidth)
+				.attr("class","svg_cercles")
+				.append("g")
+				.attr("transform", "translate(" + svgWidth/2 + "," + svgWidth/2 +")");
+
+			background = svg.append("path")
+				.datum({endAngle: τ})
+				//.attr('class', 'path_background')
+				.attr('class', class_assign+'_back')
+				.attr("d", arc);
+
+			foreground = svg.append("path")
+				.datum({endAngle: τ})
+				//.attr('class', 'path_foreground')
+				.attr('class', class_assign+'_front')
+				.attr("d", arc);
+
+			svg.append("text")
+				.attr("text-anchor", "middle")
+				.attr("font-size", fontSize)
+				.attr("dy", ".35em")
+
+			text = svg.select("text")
+				.attr('class','txt_cercle');
+
+			foreground.transition()
+				.duration(1000)
+				.call(animation, (avg * τ/max), max)
+				.each("end", myCallback);
+		}
+	}
 })();
