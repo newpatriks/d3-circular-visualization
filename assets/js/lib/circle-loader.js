20e1258c93c262981cdf61537c63ba736abfe4ab
Jordi Llobet <jllobet@vestmark.com>
2017-06-12 08:54:51 -0400

    Improvements

--- assets/js/lib/circle-loader.js
+++ assets/js/lib/circle-loader.js
@@ -1,85 +1,80 @@
-var CircleLoader = function() {
-  var _svgWidth = 300,
-    _iWidthCircle = 50,
-    τ = 2 * Math.PI,
-    _fontSize = 20,
-    _min = 0, _max = 100,
-    _value, _oldValue, _oldAngle,
-    arc, svg, background, foreground, text;
-
-  function animation(transition, newAngle, oldAngle)
-  {
-    _oldAngle = (!oldAngle) ? _min : oldAngle;
-    var interpolate = d3.interpolate(_oldAngle, newAngle);
-    var interpolateValue = d3.interpolateNumber(_oldAngle, newAngle);
-
-    transition.attrTween("d", function(d) {
-      return function(t) {
-        d.endAngle = interpolate(t);
-        var textLabels = text
-          .text( d3.format(".1f")(interpolateValue(t) * _max / τ) +' %' );
-        return arc(d);
-      };
-    });
-    _oldValue = (newAngle*_max/τ);
-  }
-  
-  function myCallback() {
-    foreground.transition().duration(500).ease("back-out").attr("transform", "scale(2.1)");
-    background.transition().duration(500).ease("back-out").attr("transform", "scale(2.1)");
-  }
-
-  return {
-    init : function(elem, class_assign, value, width, fontSize)
-    {
-      _value = value; 
-      _svgWidth = (width) ? width : _svgWidth;
-      _fontSize = fontSize ? fontSize : _fontSize;
-      _iWidthCircle = _svgWidth/6;
-      
-      arc = d3.svg.arc()
-        .innerRadius(_iWidthCircle)
-        .outerRadius(_iWidthCircle+3)
-        .startAngle(0);
-
-      svg = d3.select("#"+elem).append("svg")
-        .attr("width", _svgWidth)
-        .attr("height", _svgWidth)
-        .attr("class","svg_cercles")
-        .append("g")
-        .attr("transform", "translate(" + _svgWidth/2 + "," + _svgWidth/2 +")");
-
-      background = svg.append("path")
-        .datum({endAngle: τ})
-        .attr('class', class_assign+'_back')
-        .attr("d", arc);
-
-      foreground = svg.append("path")
-        .datum({endAngle: τ})
-        .attr('class', class_assign+'_front')
-        .attr("d", arc);
-
-      svg.append("text")
-        .attr("text-anchor", "middle")
-        .attr("font-size", _fontSize + "px")
-        .attr("dy", ".35em")
-
-      text = svg.select("text")
-        .attr('class','circle-txt');
-
-      foreground.transition()
-        .duration(1000)
-        .call(animation, (_value * τ/_max))
-        .each("end", myCallback);
-    },
-    update : function(value)
-    {
-      _value = value;
-
-      foreground.transition()
-        .duration(1000)
-        .call(animation, (_value * τ/_max), (_oldValue * τ/_max))
-        .each("end", myCallback);
-    }
-  }
+function CircleLoader() {
+	this.svgWidth = 300;
+	this.iWidthCircle = 50;
+	this.τ = 2 * Math.PI;
+	this.fontSize = 20;
+	this.min = 0;
+	this.max = 100;
 }
+
+CircleLoader.prototype = (function() {
+	var animation = function(transition, newAngle, oldAngle) {
+		this.oldAngle = (!oldAngle) ? this.min : oldAngle;
+		var interpolate = d3.interpolate(this.oldAngle, newAngle);
+		var interpolateValue = d3.interpolateNumber(this.oldAngle, newAngle);
+
+		transition.attrTween("d", function(d) {
+			return function(t) {
+				d.endAngle = interpolate(t);
+				var textLabels = text.text( d3.format(".1f")(interpolateValue(t) * this.max / this.τ) +' %' );
+				return arc(d);
+			};
+		});
+		this.oldValue = (newAngle * this.max/this.τ);
+	}
+	var callback = function() {
+		this.foreground.transition().duration(500).ease("back-out").attr("transform", "scale(2.1)");
+		this.background.transition().duration(500).ease("back-out").attr("transform", "scale(2.1)");
+	}
+
+	return {
+		init: function(elem, assignedClass, value, width, fontSize) {
+			this.value = value;
+			this.svgWidth = (width) ? width : this.svgWidth;
+			this.fontSize = fontSize ? fontSize : this.fontSize;
+			this.iWidthCircle = this.svgWidth/6;
+
+			arc = d3.svg.arc()
+				.innerRadius(this.iWidthCircle)
+				.outerRadius(this.iWidthCircle+3)
+				.startAngle(0);
+
+			svg = d3.select("#"+elem).append("svg")
+				.attr("width", this.svgWidth)
+				.attr("height", this.svgWidth)
+				.attr("class","svg_cercles")
+				.append("g")
+				.attr("transform", "translate(" + this.svgWidth/2 + "," + this.svgWidth/2 +")");
+
+			background = svg.append("path")
+			.datum({endAngle: this.τ})
+			.attr('class', assignedClass+'_back')
+			.attr("d", arc);
+
+			foreground = svg.append("path")
+				.datum({endAngle: this.τ})
+				.attr('class', assignedClass+'_front')
+				.attr("d", arc);
+
+			svg.append("text")
+				.attr("text-anchor", "middle")
+				.attr("font-size", this.fontSize + "px")
+				.attr("dy", ".35em")
+
+			text = svg.select("text")
+				.attr('class','circle-txt');
+
+			foreground.transition()
+				.duration(1000)
+				.call(animation, (this.value * this.τ/this.max))
+				.each("end", callback);
+		},
+		update: function(value) {
+			this.value = value;
+			foreground.transition()
+				.duration(1000)
+				.call(animation, (this.value * this.τ/this.max), (this.oldValue * τ/this.max))
+				.each("end", callback);
+		}
+	}
+})();
